9cb88202cae43749426ce0b9fd7b037dc53af288
adhithyanms <adhithyanshanmugam@gmail.com>
2026-02-12 13:51:19 +0530

    Backend deploy

--- Backend/server.js
+++ Backend/server.js
@@ -32,10 +32,12 @@ mongoose.connect(mongoURI)
   });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if(require.main===module){
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port locally ${PORT}`);
+  });
+}
 
 module.exports = app;
 
